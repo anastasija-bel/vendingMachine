3e5cea59ef96a29e9d069b1a5f829e7434210e09
anastasija-bel <anastasija.belaka@gmail.com>
2025-04-30 23:03:46 +0100

    Add vending machine and launch option

---
+++ launch.py
@@ -0,0 +1,99 @@
+# IMPORT STANDARD LIBRARIES
+import os
+import logging
+
+# IMPORT LOCAL LIBRARIES
+import vending_machine as vm
+
+
+LOG = logging.getLogger(__name__)
+# type: dict[int,int]
+# dictionary that maps a user choice to coins value
+COINS = {"1":1, "2":2, "3":5, "4":10, "5":"Next", "6":0}
+WATER = frozenset(["1", "2", "3"])
+
+
+def choose_drink(vending_machine, total_coins):
+
+    drink_chosen = False
+    change = 0
+
+    if not total_coins:
+        print("\nPlease insert money to buy a drink")
+        return change
+
+    print("\nChoose your drink")
+    print("| 1 - Still Water (30) | 2 - Fizzy Water (35) | 3 - Cancel |")
+
+    while not drink_chosen:
+        water_option = input()
+
+        if water_option not in WATER:
+            print("\nPlease choose your drink from available options")
+            continue
+
+        if water_option == "3":
+            break
+        elif water_option == "2":
+            water_cost = vm.fizzyWater().price
+            water_type = "fizzy"
+        elif water_option == "1":
+            water_cost = vm.stillWater().price
+            water_type = "still"
+
+        if not vending_machine.check_inventory(water_type):
+            break
+        
+        print("Drink cost: ", water_cost)
+        if water_cost > total_coins:
+            print("\nPlease add more money")
+        else:
+            change = total_coins - water_cost
+            print("Change: ", change)
+            total_coins = change
+            vending_machine.update_inventory(water_type)
+        drink_chosen = True
+
+    return total_coins
+
+
+def main():
+    # logging.basicConfig(filename='vending_machine.log', level=logging.INFO)
+    # logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+    logging.basicConfig(level=logging.INFO)
+
+    vending_machine = vm.vendingMachine()
+    vending_machine.init_default_settins()
+
+    total_coins = 0
+    done = False
+
+    print("\nPlease choose an option to add a coin")
+
+    while not done:
+        print(vending_machine.get_current_inventory())
+        # print("+", "-" * 58, "+")
+        print("| 1 - 1c | 2 - 2c | 3 - 5c | 4 - 10c | 5 - Next | 6 - Cancel |")
+        # print("+", "-" * 58, "+")
+
+        print("Total: ", total_coins)
+        option = input()
+
+        if option not in COINS.keys():
+            print("Please choose the available option")
+            continue
+
+        coin_added = COINS[option]
+
+        if not coin_added:
+            print("Here is your change: ", total_coins)
+            done = True
+
+        if isinstance(coin_added, str):
+            total_coins = choose_drink(vending_machine, total_coins)
+        else:
+            total_coins += coin_added
+
+
+if __name__ == "__main__":
+    main()
---
+++ vending_machine.py
@@ -0,0 +1,44 @@
+class vendingMachine():
+
+    def __init__(self, parent=None):
+        self.total_bottles_count = 5
+        self.inventory = {"total": 0, "still": 0, "fizzy": 0}
+
+    def init_default_settins(self):
+        self.still_water_count = stillWater().count
+        self.fizzy_water_count = fizzyWater().count
+        self.inventory = {"total": self.total_bottles_count, 
+                         "still": self.still_water_count, 
+                         "fizzy": self.fizzy_water_count}
+    
+    def update_inventory(self, water_type):
+        current_count = self.inventory[water_type]
+        current_total_count = self.inventory["total"]
+        updated_count = current_count - 1
+        updated_total_count = current_total_count - 1
+        self.inventory[water_type] = updated_count
+        self.inventory["total"] = updated_total_count
+
+    def get_current_inventory(self):
+        return self.inventory
+    
+    def check_inventory(self, water_type):
+        if self.inventory[water_type]:
+            return True
+        print("Oh no! This option is out of stock. Please try a different option")
+
+
+class stillWater(vendingMachine):
+
+    def __init__(self):
+        super(stillWater, self).__init__()
+        self.count = int(2/5*self.total_bottles_count)
+        self.price = 30
+
+
+class fizzyWater(vendingMachine):
+
+    def __init__(self):
+        super(fizzyWater, self).__init__()
+        self.count = int(3/5*self.total_bottles_count)
+        self.price = 35
