338739dd0e9b8441baf0461251555fd057e08441
anastasija-bel <anastasija.belaka@gmail.com>
2025-05-01 14:08:32 +0100

    Add constants

---
+++ python/constants.py
@@ -0,0 +1,8 @@
+# dict[str, int|str]: dictionary that maps a user choice to coins value
+COINS = {"1":1, "2":2, "3":5, "4":10, "5":"Next", "6":0}
+# dict[int, str|int]: dictionary that maps a user choice of water type
+WATER = {"1": "still", "2": "fizzy", "3": 0}
+# str: water bottles cost key for inventory
+COST = "cost"
+# str: water bottles count key for inventory
+COUNT = "count"
