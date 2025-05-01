14d8837e3f43cbab4fe6a38e82ab2f2293fdcabf
anastasija-bel <anastasija.belaka@gmail.com>
2025-05-01 16:22:08 +0100

    Add docstrings and remove typo

--- python/launch.py
+++ python/launch.py
@@ -1,10 +1,21 @@
+'''A base implementation of logic for buying a bottle of water from a vending machine.'''
 # IMPORT LOCAL LIBRARIES
 import vending_machine as vm
 import constants as const
 
 
 def choose_drink(vending_machine, total_coins):
+    '''Choose a drink from available options in a vending machine.
+        
+        Args:
+            vending_machine (vending_machine.vendingMachine): 
+                Vending machine object with available information and actions for inventory and its cost/count.
+            total_coins (int):
+                All coins added to a vending machine for buying a drink.
 
+        Returns:
+            int: Change after purchasing a drink (or all coins if cancelled)
+    '''
     drink_chosen = False
     change = 0
 
@@ -23,7 +34,7 @@ def choose_drink(vending_machine, total_coins):
             continue
 
         water_type = const.WATER[water_option]
-        # cancel if a user chooses 3rd ("Cancel") option
+        # Return to adding coins if a user chooses 3rd ("Cancel") option
         if not isinstance(water_type, str):
             break
         
@@ -49,9 +60,9 @@ def choose_drink(vending_machine, total_coins):
 
 
 def main():
-
+    # Initialise default settings for a vending machine
     vending_machine = vm.vendingMachine()
-    vending_machine.init_default_settins()
+    vending_machine.init_default_settings()
 
     total_coins = 0
     done = False
@@ -71,10 +82,12 @@ def main():
 
         coin_added = const.COINS[option]
 
+        # Stop if a user chooses 6th ("Cancel") option
         if not coin_added:
             print("Here is your change: ", total_coins)
             done = True
 
+        # Continue to selecting a drink when a user chooses 5th ("Next") option
         if isinstance(coin_added, str):
             total_coins = choose_drink(vending_machine, total_coins)
         else:
--- python/vending_machine.py
+++ python/vending_machine.py
@@ -1,14 +1,16 @@
+'''A base implementation of a vending machine settings and its inventory behaviour.'''
 # IMPORT LOCAL LIBRARIES
 import constants as const
 
 
 class vendingMachine():
-
+    '''Vending machine with its inventory.'''
     def __init__(self, parent=None):
         self.total_bottles_count = 500
         self.inventory = {const.TOTAL: 0, const.STILL: 0, const.FIZZY: 0}
 
-    def init_default_settins(self):
+    def init_default_settings(self):
+        '''Initialize default inventory of a vending machine.'''
         self.still_water_count = stillWater().count
         self.fizzy_water_count = fizzyWater().count
         self.still_water_cost = stillWater().price
@@ -20,6 +22,7 @@ class vendingMachine():
                                    const.COST: self.fizzy_water_cost}}
     
     def update_inventory(self, water_type):
+        '''Update current inventory after drink purchase.'''
         current_count = self.inventory[water_type][const.COUNT]
         current_total_count = self.inventory[const.TOTAL]
         updated_count = current_count - 1
@@ -28,22 +31,25 @@ class vendingMachine():
         self.inventory[const.TOTAL] = updated_total_count
 
     def get_current_inventory(self):
+        '''dict{str: {str: int}}: Get currently available inventory of a vending machine.'''
         return self.inventory
     
     def show_current_inventory(self):
+        '''Show a user currently available inventory of a vending machine.'''
         print("\n| Inventory : Total - %s | Still Water - %s | Fizzy - %s |" % (
             self.inventory[const.TOTAL], 
             self.inventory[const.STILL][const.COUNT], 
             self.inventory[const.FIZZY][const.COUNT]))
     
     def check_inventory(self, water_type):
+        '''bool or None: Check if the inventory is empty.'''
         if self.inventory[water_type][const.COUNT]:
             return True
         print("Oh no! This option is out of stock. Please try a different option")
 
 
 class stillWater(vendingMachine):
-
+    '''A bottle of still water in a vending machine.'''
     def __init__(self):
         super(stillWater, self).__init__()
         self.count = int(2/5*self.total_bottles_count)
@@ -51,7 +57,7 @@ class stillWater(vendingMachine):
 
 
 class fizzyWater(vendingMachine):
-
+    '''A bottle of fizzy water in a vending machine.'''
     def __init__(self):
         super(fizzyWater, self).__init__()
         self.count = int(3/5*self.total_bottles_count)
