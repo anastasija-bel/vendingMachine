01287eea9c8cf35f2ee7ada7632535b8aae2aee9
anastasija-bel <anastasija.belaka@gmail.com>
2025-05-01 14:32:25 +0100

    Update constants

--- python/constants.py
+++ python/constants.py
@@ -1,8 +1,14 @@
-# dict[str, int|str]: dictionary that maps a user choice to coins value
-COINS = {"1":1, "2":2, "3":5, "4":10, "5":"Next", "6":0}
-# dict[int, str|int]: dictionary that maps a user choice of water type
-WATER = {"1": "still", "2": "fizzy", "3": 0}
+# str: all water bottles count key for inventory
+TOTAL = "total"
+# str: still water bottles total count key for inventory
+STILL = "still"
+# str: fizzy water bottles total count key for inventory
+FIZZY = "fizzy"
 # str: water bottles cost key for inventory
 COST = "cost"
 # str: water bottles count key for inventory
 COUNT = "count"
+# dict[str, int|str]: dictionary that maps a user choice to coins value
+COINS = {"1":1, "2":2, "3":5, "4":10, "5":"Next", "6":0}
+# dict[int, str|int]: dictionary that maps a user choice of water type
+WATER = {"1": STILL, "2": FIZZY, "3": 0}
